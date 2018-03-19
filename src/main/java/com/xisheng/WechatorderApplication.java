6a7696f689220f7d2ab2c6f6e5c0d398c277e5ec
zhengxs <gdpuzxs@163.com>
2018-03-19 17:36:00 +0800

    wechat order first commit

---
+++ src/main/java/com/xisheng/WechatorderApplication.java
@@ -0,0 +1,12 @@
+package com.xisheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatorderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xisheng/WechatorderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xisheng;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatorderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
